aedecc39689e8a1b2d307e3d414ae4a50b75fb74
thisisbatra <batranitin118@gmail.com>
2024-05-29 20:17:29 +0530

    gifs n stickers functionality completed!

--- src/Components/ImgContainer/ImgContainer.jsx
+++ src/Components/ImgContainer/ImgContainer.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import styles from './ImgContainer.module.css';
 const ImgContainer = (props) => {
     return (
-        <div className={styles.cont}>
+        props.resource==="gifs"?
+            <div className={styles.cont}>
             <img src={props.src} alt={props.alt} className={styles.img} />
-        </div>
+            </div>
+            :
+            <div className={`${styles.cont} ${styles.contWithStickers}`}>
+            <img src={props.src} alt={props.alt} className={styles.img} />
+            </div>
     );
 }
 
---
+++ src/Components/SkeletonContainer/SkeletonContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './SkeletonContainer.module.css';
+
+const SkeletonContainer = () => {
+    return (
+        <div className={styles.cont}>
+            
+        </div>
+    );
+}
+
+export default SkeletonContainer;
