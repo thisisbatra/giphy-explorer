ec943b09258faac6494339521de7248b8845fe1b
thisisbatra <batranitin118@gmail.com>
2024-05-29 23:18:44 +0530

    somechanges

--- src/Components/Head/Head.jsx
+++ src/Components/Head/Head.jsx
@@ -13,13 +13,13 @@ const Head = () => {
                     <img src="./assets/logo/GIPHY Icon DarkBackgrounds 45.png" alt="logo" />
                     <h1>GIPHY</h1>
                 </span>
-                <ul className={styles.Keyword}>
+                {/* <ul className={styles.Keyword}>
                     <li><CiMenuKebab /></li>
                     <li><Keywords Text="Reaction" clr="#02caff" gradient="linear-gradient(to right,  #02caff ,#2ba0ff)" /></li>
                     <li><Keywords Text="Entertainment" clr="#7459ff" gradient="linear-gradient(to right,  #3696ff,#7459ff)" /></li>
                     <li><Keywords Text="Sports" clr="#b239e6" gradient="linear-gradient(to right,  #9c34fd ,#b239e6)" /></li>
                     <li><Keywords Text="Meme" clr="#de44be" gradient="linear-gradient(to right,  #ba3be0 ,#de44be)" /></li>
-                </ul>
+                </ul> */}
                 {/* <div className={styles.user}>
                     <CgBoy />
                     <CgGirl />
